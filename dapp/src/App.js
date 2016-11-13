c4fdbac44c753d13f6500f5a814b2d342cfcbd0c
Jay Kruer <jaykru@me.com>
2016-11-13 06:21:51 -0600

    added a burger menu; mostly scaffolding.

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,59 +1,99 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import BurgerMenu from 'react-burger-menu';
+import Web3 from 'web3';
 import './App.css';
 
-class NameForm extends React.Component {
-  constructor() {
-    super();
-    this.state = {value: ''};
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+let McMenu = React.createClass({
 
-  handleSubmit(/*event*/) {
-    super.state.value = this.state.value;
-    //event.preventDefault();
-  }
+  getInitialState() {
+    return {hidden : false};
+  },
+
+    show() {
+    this.setState({hidden : false});
+  },
 
   render() {
+    let style;
+
+    if (this.state.hidden) {
+      style = {display: 'none'};
+    }
+
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" value={this.state.value}/>
-        <input type="submit" value="OK"/>
-      </form>
+      <div style={style} className={this.props.side}>
+        {this.props.children}
+      </div>
     );
   }
-}
+});
 
-class Name extends Component {
+
+class Login extends React.Component {
   constructor() {
     super();
-    this.state = {
-      value: "Uninitialized. Register: "
-    };
+    this.state = {field:'',name: '', loggedin:false};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+  
+  handleChange(event) {
+    this.setState({field: event.target.value,name:event.target.value,loggedin:false});
+  }
+
+  handleSubmit(event) {
+    this.setState({field: '',name: this.state.field, loggedin: true});
+    event.preventDefault();
+  }
+
   render() {
-    if (localStorage.ethname) {
-      this.state.value = localStorage.ethname;
-      return(
+    if(!this.state.loggedin) {
+         return (
         <div>
-        <h1>{this.state.value}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.field} onChange={this.handleChange}/>
+          <p>Name: {this.state.name}</p>
+          <input type="submit" value="OK"/>
+        </form>
         </div>
       );
     }
     else {
-      return(
+       return (
         <div>
-        <p>{this.state.value}</p>
-        <NameForm/>
+        <p>{this.state.name}</p>
         </div>
       );
-    }
-   }
+
+       }
+  }
 }
 
 class App extends Component {
+  getMenu() {
+    const Menu = BurgerMenu['slide'];
+    const items = this.getItems();
+    return (<McMenu wait={20}>
+          <Menu id="meme" pageWrapId={'page-wrap'} outerContainerId={'outer-container'}>
+            {items}
+          </Menu>
+        </McMenu>);
+  };
+
+  getItems() {
+    return([<a id="home" className="menu-item" href="/">Home</a>,
+        <a id="about" className="menu-item" href="/about">About</a>,
+        <a id="contact" className="menu-item" href="/contact">Contact</a>]);
+  };
+
   render() {
-    <Name/>
+    return(/*<Login/>*/
+      <div id="outer-container" style={{height: '100%'}}>
+        {this.getMenu()}
+        <main id="page-wrap">
+          <h1>BEEG</h1>
+        </main>
+      </div>)
   }
 };
 
