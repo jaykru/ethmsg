8ac641678dc867b8f0aa89677bf0bd89db8bc75b
Jay Kruer <jaykru@me.com>
2016-11-13 03:01:50 -0600

    Added contract code and react template for building dapp.

---
+++ ethmsgcontract.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.2;
+
+contract Messenger {
+
+    struct Message {
+        address sender;
+        string text;
+        uint timestamp;
+    }
+
+    mapping (address => Message[]) public messages;
+
+    function sendMessage(address to, string talk) returns (bool) {
+        if (to != msg.sender) {
+            messages[to].push(Message({
+                sender: msg.sender,
+                text: talk,
+                timestamp: now
+            }));
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    function numMessages() returns (uint) {
+        return messages[msg.sender].length;
+    }
+
+    function getMessage(uint index) returns (address sender, string text, uint timestamp) {
+        Message retmsg = messages[msg.sender][index];
+        return (retmsg.sender,retmsg.text,retmsg.timestamp);
+    }
+}
