c0968d3ff6f35898aef054e40a7be034ac02a514
Jay Kruer <jaykru@me.com>
2016-11-13 20:14:08 -0500

    fug

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -4,51 +4,64 @@ import Web3 from 'web3';
 import AutoScroll from 'react-auto-scroll';
 import './App.css';
 
-var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-if(!web3.isConnected()) {
-  alert('shit');
+  // set the provider you want from Web3.providers
+  var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9001"));
+if (!web3.isConnected()) {
+  alert("shit")
 }
-
 var abi = [{ "constant": false, "inputs": [ { "name": "to", "type": "uint256" }, { "name": "from", "type": "uint256" }, { "name": "talk", "type": "string" } ], "name": "sendMessage", "outputs": [], "payable": false, "type": "function" }, { "constant": false, "inputs": [], "name": "userAdd", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" } ], "name": "messages", "outputs": [ { "name": "sender", "type": "uint256" }, { "name": "recipient", "type": "uint256" }, { "name": "text", "type": "string" }, { "name": "timestamp", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "user", "type": "uint256" }, { "name": "index", "type": "uint256" } ], "name": "getMessage", "outputs": [ { "name": "sender", "type": "uint256" }, { "name": "text", "type": "string" }, { "name": "timestamp", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "user", "type": "uint256" } ], "name": "numMessages", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }];
 
 var ethmsgContract = web3.eth.contract(abi);
 var ethmsg = ethmsgContract.at('0x7f7c972D7610C6c242e861785447E2efC9E826fd');
 
-class MessageListing extends AutoScroll, Component {
+class MessageListing extends AutoScroll {
   constructor() {
     super();
-    this.state = {from:'', to: '', pnumber:0,cnumber:0,messages:[]};
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {from:'', to: '', pnumberh:0,cnumberh:0,pnumbert:0,cnumbert:0,messages:[]};
   }
   
   getNumber() {
-    var howmany = ethmsg.numMessages(this.to);
-    this.setState({pnumber:this.state.cnumber,cnumber:howmany});
+    var howmanyhere = ethmsg.numMessages(this.to);
+    var howmanythere = ethmsg.numMessages(this.from);
+    this.setState({pnumberh:this.state.cnumberh,cnumberh:howmanyhere});
+    this.setState({pnumbert:this.state.cnumbert,cnumbert:howmanythere});
   }
 
   getMessages() {
-    if (cnumber > pnumber) {
-      var newmessages = [];
-      for (var i = 0; i<cnumber; i++) {
+    var newmessages = [];
+    if (this.state.cnumberh > this.state.pnumberh) {
+      for (var i = 0; i<this.state.cnumberh; i++) {
         var msg = ethmsg.getMessage(this.state.to,i);
         newmessages.push(msg);
       }
-      this.setState({messages:newmessages});
+    if (this.state.cnumbert > this.state.pnumbert) {
+      for (var j = 0; j<this.state.cnumbert; j++) {
+        var msg1 = ethmsg.getMessage(this.state.from,j);
+        newmessages.push(msg1);
+      }
+    for (var g = 1; g<newmessages.length; g++) {
+      if (newmessages[g].timestamp > newmessages[g-1].timestamp) {
+        var k = newmessages[g];
+        newmessages[g] = newmessages[g-1];
+        newmessages[g-1] = k;
+      }
     }
+    this.setState({messages:newmessages});
+    }
+  }
   }
 
   render() {
-    getNumber()
-    getMessages()
+    this.getNumber()
+    this.getMessages()
     var msgblocks = [];
-    this.state.messages.forEach(item) {
+    this.state.messages.forEach(function (item) {
       msgblocks.push(<div><p>{item.sender}:</p>
                 <pre>
                   {item.text}
                 </pre>
                 </div>)
-    }
+    });
     return(msgblocks);
    }
 }
@@ -78,62 +91,32 @@ let McMenu = React.createClass({
   }
 });
 
-class Login extends React.Component {
+class App extends Component {
   constructor() {
     super();
-    this.state = {field:'',name: '', loggedin:false};
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  
-  handleChange(event) {
-    this.setState({field: event.target.value,name:event.target.value,loggedin:false});
+    this.state={uid:ethmsg.userAdd(),curchat:0,cnum:0,pnum:0};
   }
 
-  handleSubmit(event) {
-    this.setState({field: '',name: this.state.field, loggedin: true});
-    event.preventDefault();
-  }
-
-  render() {
-    if(!this.state.loggedin) {
-         return (
-        <div>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.field} onChange={this.handleChange}/>
-          <p>Name: {this.state.name}</p>
-          <input type="submit" value="OK"/>
-        </form>
-        </div>
-      );
-    }
-    else {
-       return (
-        <div>
-        <p>{this.state.name}</p>
-        </div>
-      );
-
-       }
-  }
-}
-
-class App extends Component {
   getMenu() {
     const Menu = BurgerMenu['slide'];
-    const items = this.getItems();
-    return (<McMenu wait={20}>
+    return (
+        <McMenu wait={20}>
           <Menu id="meme" pageWrapId={'page-wrap'} outerContainerId={'outer-container'}>
-            {items}
+            {this.getItems()}
           </Menu>
-        </McMenu>);
-  };
+        </McMenu>
+     );
+  }
 
   getItems() {
-    return([<a id="home" className="menu-item" href="/">Home</a>,
-        <a id="about" className="menu-item" href="/about">About</a>,
-        <a id="contact" className="menu-item" href="/contact">Contact</a>]);
-  };
+    return(
+      <div>
+      <button onClick={this.setState({curchat:0})}>sent</button>
+      <br/>
+      <button onClick={this.setState({curchat:1})}>jeremy</button>
+      </div>
+    );
+  }
 
   render() {
     return(/*<Login/>*/
@@ -141,6 +124,7 @@ class App extends Component {
         {this.getMenu()}
         <main id="page-wrap">
           <h1>dischat</h1>
+          <MessageListing from={this.state.curchat} to={this.state.uid}/>
         </main>
       </div>)
   }
